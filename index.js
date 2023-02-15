1ba8f5df60620c24f08bc5dd34bb74ab050b6409
RAnastasiya <rizhko.anastasiya@gmail.com>
2023-02-15 18:53:04 +0200

    Add HW

--- index.js
+++ index.js
@@ -1,30 +1,5 @@
 "use strict";
 
-// const getFactorial = (number) => {
-//   if (typeof number !== "number") throw new TypeError("number must be number");
-//   if (number > 10000 || number < 0)
-//     throw new RangeError("number must be 0..10000");
-//   if (number === 0) return 1;
-//   return number * getFactorial(number - 1);
-// };
-
-// try {
-//   console.log(getFactorial(5));
-// } catch (error) {
-//   console.log(error);
-// } finally {
-//   console.log('finally')
-// }
-
-// const power = (base, exp) => {
-//   if (exp < 0) {
-//     return 1 / power(base, -exp);
-//   }
-//   if (exp === 0) return 1;
-//   return base * power(base, exp - 1);
-// };
-// console.log(power(4, -4));
-
 /*
 написати рекурсивну функцію, яка приймає число і повертає рядок з вкладених пар круглих дужок.
 кількість пар дорівнює прийнятому аргументу.
@@ -49,6 +24,7 @@ try {
   console.log("finally");
 }
 
+
 /*
 написати рекурсивну функцію, яка приймає два числа і повертає найбільший спільний дільник цих двох чисел
 // getDivider(24, 15) -> 3
@@ -58,3 +34,23 @@ try {
 Алгоритм Евкліда дозволяє знайти НСД двох натуральних чисел.
 Суть алгоритму Евкліда – два числа порівнюють і від більшого віднімають менше до тих пір, поки числа не стануть рівними. Число, якому вони стануть рівними, і є їх найбільший спільний дільник.
  */
+
+const getDivider = (num1, num2) => {
+  if(typeof num1 !== 'number' || typeof num2 !== 'number') {
+    throw new TypeError("num1 and num2 must be number");
+  }
+  if(num1<0) return getDivider(-num1, num2)
+  if(num2<0) return getDivider(num1, -num2)
+  if (num1 === num2) return num1
+  return getDivider(num1>num2?num1-num2:num1, num2>num1?num2-num1:num2)
+}
+
+try {
+  console.log(getDivider(-24, -15));
+  console.log(getDivider(3, 3));
+  console.log(getDivider(7, 3));
+} catch (error) {
+  console.log(error);
+} finally {
+  console.log("finally");
+}
